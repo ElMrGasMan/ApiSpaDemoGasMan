e0d3b0f71f6d138cdfd6e93c8674b66cdd906d56
Agustin Schipani <agus-calderon-schip03@outlook.com>
2024-10-28 00:59:41 -0300

    Update EmailService.cs
    Odio las CORS

--- ApiSpaDemo/Services/EmailService.cs
+++ ApiSpaDemo/Services/EmailService.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Cors;
 
 namespace ApiSpaDemo.Services
 {
-    [EnableCors("ReglasCors")]
+    [EnableCors("PermitirTodo")]
     public class EmailService
     {
         private readonly SMTPSettingsModel _smtpSettingsModel;
